6bbedb9f2ad8175cf15ad4c60d81f0c51e6ef2c3
NikhilA2406 <NikhiljaiswalT@gmail.com>
2024-08-16 22:35:03 +0530

    First Commit

---
+++ src/main/java/firstservlet.java
@@ -0,0 +1,41 @@
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/firstservlet")
+public class firstservlet extends HttpServlet 
+{
+	private static final long serialVersionUID = 1L;
+	public firstservlet()
+	{
+		System.out.println("Servlet obj is created internally by container");
+	}
+
+	
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		String name=request.getParameter("fullname");
+		String email=request.getParameter("email");
+		
+		PrintWriter writer=response.getWriter();
+		
+		writer.println("<html><head><title> Second App</title></head></html>");
+		writer.println("<body bgcolor='cyan'> <h1><marquee> Welcome to our dynamic app</marquee></h1>"); 
+		writer.println("<table>");
+		writer.println("<tr> <th>NAME</th>  <th>EMAIL</th></tr>");
+		writer.println("<tr> <td>"+name+"</td> <td>"+email+"</td></tr>");
+		writer.println("</table></body>");
+		writer.println("</html>");
+		
+		
+		writer.close();
+	
+	}
+
+}
